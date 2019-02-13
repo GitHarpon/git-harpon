387b38466718b79259b2770484b2e524ca073992
Clément Drouin <clement.drouin@etu.univ-rouen.fr>
2019-02-13 17:19:07 +0100

    maintain(test): intégration des TU + Coverage + template de test

--- src/app/components/input/input.component.spec.ts
+++ src/app/components/input/input.component.spec.ts
@@ -1,25 +1,62 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync} from '@angular/core/testing';
 
 import { InputComponent } from './input.component';
+import { FormsModule } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+class MockTranslateService {
+  instant(text: string) {
+    return text;
+  }
+}
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
+  let inputEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
+      declarations: [InputComponent],
+      imports: [
+        FormsModule
+      ],
+      providers: [
+        {
+          provide: TranslateService,
+          useClass: MockTranslateService
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    inputEl = fixture.debugElement.query(By.css('input.gh-input'));
   });
 
-  it('should create', () => {
+  it('tests the component creation', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('tests the component value', fakeAsync(() => {
+    const content = 'axuluphrum';
+    component.value = content;
+    fixture.detectChanges();
+    inputEl.nativeElement.dispatchEvent(new Event('input'));
+    tick();
+    fixture.detectChanges();
+    expect(inputEl.nativeElement.value).toEqual(content);
+  }));
+
+  it('tests the getPlaceholderTranslation function', () => {
+    const content = 'something';
+    component.placeholder = content;
+    const translation = component.getPlaceholderTranslation();
+    expect(translation).toBe(component.placeholder.toUpperCase());
   });
 });
--- src/app/components/modal/modal.component.spec.ts
+++ src/app/components/modal/modal.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+
+let translations: any = {"CARDS_TITLE": "This is a test"};
+
+class MockTranslateLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    return translations;
+  }
+}
+
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
   let fixture: ComponentFixture<ModalComponent>;
+  let modalEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModalComponent ]
+      declarations: [ ModalComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: MockTranslateLoader},
+        })
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +36,29 @@ describe('ModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    modalEl = fixture.debugElement.query(By.css('div.gh-modal'));
   });
 
-  it('should create', () => {
+  it('tests the component creation', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('tests the visible property with true value', () => {
+    component.visible = true;
+    fixture.detectChanges();
+    const classList = modalEl.nativeElement.classList;
+    expect(classList).toContain('visible');
+  });
+
+  it('tests the visible property with false value', () => {
+    component.visible = false;
+    fixture.detectChanges();
+    const classList = modalEl.nativeElement.classList;
+    expect(classList.contains('visible')).toBeFalsy();
+  });
+
+  it('tests the openModal function', () => {
+    component.closeModal();
+    expect(component.visible).toBeFalsy();
   });
 });
