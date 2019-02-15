356a828cfd35113d3aff05a069ed6338aab69501
Clément Drouin <clement.drouin@etu.univ-rouen.fr>
2019-02-15 14:44:56 +0100

    feat(pref-lang): début du changement de langue

--- src/app/screens/preferences/preferences.component.ts
+++ src/app/screens/preferences/preferences.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-preferences',
@@ -10,17 +11,38 @@ export class PreferencesComponent implements OnInit {
 
   preferencesVisible: Boolean;
   preferencesTabSelectedIndex: any;
+  dropdownLanguageValue: String;
+  dataDropdownLanguage: Array<any>;
 
-  constructor(public router: Router) { }
+  key: String = 'key';
+  value: String = 'value';
+
+
+  constructor(public router: Router, private translate: TranslateService) {
+  }
 
   ngOnInit() {
     this.preferencesVisible = true;
     this.preferencesTabSelectedIndex = 1;
+    this.dropdownLanguageValue = 'Français';
+
+    this.dataDropdownLanguage = [
+      {key: 'en', value: 'English'},
+      {key: 'fr', value: 'Français'},
+    ];
   }
 
   checkIfCloseModal(event) {
     if (event.index === 0) {
       this.router.navigate(['home']);
+    }
+  }
+
+  switchLanguage() {
+    if (this.dropdownLanguageValue === 'Français') {
+      this.translate.use('fr');
+    } else {
+      this.translate.use('en');
     }
   }
 }
