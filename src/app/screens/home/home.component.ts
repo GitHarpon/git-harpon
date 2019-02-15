a53a4180cf16add24ca1b4ac78cddb6176d05879
Martin <martinblondel@hotmail.fr>
2019-02-15 18:36:51 +0100

    feat(clone): avancement partie HTML et début event

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { ElectronService } from '../../providers/electron.service';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +12,9 @@ export class HomeComponent implements OnInit {
 
   projectModalVisible: Boolean;
   searchInputValue: String;
+  destination: string;
 
-  constructor(public router: Router, private toastr: ToastrService) { }
+  constructor(public router: Router, private toastr: ToastrService, private electronService: ElectronService) { }
 
   ngOnInit() {
 
@@ -46,4 +48,13 @@ export class HomeComponent implements OnInit {
     this.toastr.info(this.searchInputValue.toString());
   }
 
+  browse() {
+    const NEWPATH = this.electronService.remote.dialog.showOpenDialog({
+      properties: ['openDirectory']
+    });
+    if (NEWPATH !== undefined) {
+      this.destination = NEWPATH[0];
+    }
+
+  }
 }
