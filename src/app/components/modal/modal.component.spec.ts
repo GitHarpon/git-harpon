f35b624737b4669192509dca9af9c990b0d40517
Clément Drouin <clement.drouin@etu.univ-rouen.fr>
2019-02-16 14:42:22 +0100

    test(template): réajout d'un template de test

---
+++ src/app/components/modal/modal.component.spec.ts
@@ -0,0 +1,55 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalComponent } from './modal.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { MockTranslateLoader } from '../../models/MockTranslateLoader';
+import { LoaderComponent } from '../loader/loader.component';
+
+describe('ModalComponent', () => {
+  const component: ModalComponent;
+  const fixture: ComponentFixture<ModalComponent>;
+  const modalEl: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ModalComponent, LoaderComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: MockTranslateLoader},
+        })
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModalComponent);
+    component = fixture.componentInstance;
+    modalEl = fixture.debugElement.query(By.css('div.gh-modal'));
+  });
+
+  it('tests the component creation', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('tests the visible property with true value', () => {
+    component.visible = true;
+    fixture.detectChanges();
+    const classList = modalEl.nativeElement.classList;
+    expect(classList).toContain('visible');
+  });
+
+  it('tests the visible property with false value', () => {
+    component.visible = false;
+    fixture.detectChanges();
+    const classList = modalEl.nativeElement.classList;
+    expect(classList.contains('visible')).toBeFalsy();
+  });
+
+  it('tests the openModal function', () => {
+    component.closeModal();
+    expect(component.visible).toBeFalsy();
+  });
+});
