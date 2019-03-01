4ee0c3c871a5983c5e5fd00b137d45aa4fb1e63f
Clément Drouin <clement.drouin@etu.univ-rouen.fr>
2019-03-01 22:56:48 +0100

    maintain(general): réajout de 2 classes de TU, fix translate, cache travis

---
+++ src/app/components/input/input.component.spec.ts
@@ -0,0 +1,63 @@
+import { async, ComponentFixture, TestBed, tick, fakeAsync} from '@angular/core/testing';
+
+import { InputComponent } from './input.component';
+import { FormsModule } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MockTranslateService } from '../../models/MockTranslateService';
+import { ThemePreferencesService } from '../../providers/theme-preferences.service';
+import { MockThemePreferencesService } from '../../models/MockThemePreferencesService';
+
+describe('InputComponent', () => {
+  let component: InputComponent;
+  let fixture: ComponentFixture<InputComponent>;
+  let inputEl: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [InputComponent],
+      imports: [
+        FormsModule
+      ],
+      providers: [
+        {
+          provide: TranslateService,
+          useClass: MockTranslateService
+        },
+        {
+          provide: ThemePreferencesService,
+          useClass: MockThemePreferencesService
+        }
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InputComponent);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('input.gh-input'));
+  });
+
+  it('tests the component creation', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('tests the component value', fakeAsync(() => {
+    const Content = 'axuluphrum';
+    component.value = Content;
+    fixture.detectChanges();
+    inputEl.nativeElement.dispatchEvent(new Event('input'));
+    tick();
+    fixture.detectChanges();
+    expect(inputEl.nativeElement.value).toEqual(Content);
+  }));
+
+  it('tests the getPlaceholderTranslation function', () => {
+    const Content = 'something';
+    component.placeholder = Content;
+    const Translation = component.getPlaceholderTranslation();
+    expect(Translation).toBe(component.placeholder.toUpperCase());
+  });
+});
