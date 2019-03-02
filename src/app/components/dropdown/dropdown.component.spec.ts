179a629083eebf77705ba3660dd881bce87ed38d
Antoine Guillory <antoineguillory@hotmail.fr>
2019-03-02 19:16:25 +0100

    test(dropdown): full coverage sur les dropdown

--- src/app/components/dropdown/dropdown.component.spec.ts
+++ src/app/components/dropdown/dropdown.component.spec.ts
@@ -43,16 +43,21 @@ describe('DropdownComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('tests the component value and accessors', fakeAsync(() => {
+  it('tests the component setter and getter', fakeAsync(() => {
+    const Content = 'axuluphrum';
+    component.value = Content;
+    expect(component.value).toEqual(Content);
+  }));
+
+  it('tests the component getter with empty value', fakeAsync(() => {
     const Content = 'axuluphrum';
     const Empty = '';
+
+    component.required = true;
     component.value = Content;
-    fixture.detectChanges();
-    expect(component.value).toEqual(Content);
-    // faire deux fonction ici en changeant nom a cahque fois
     component.value = Empty;
-    fixture.detectChanges();
-    expect(component.value).toEqual(Empty);
+
+    expect(component.value).toEqual(Content);
   }));
 
   it('tests the getOptKey function with valid idKey', fakeAsync( () => {
@@ -60,6 +65,15 @@ describe('DropdownComponent', () => {
     const Expected = Opt.key;
 
     component.idKey = 'key';
+
+    const Result = component.getOptKey(Opt);
+
+    expect(Result).toEqual(Expected);
+  }));
+
+  it('tests the getOptKey function with invalid idKey', fakeAsync( () => {
+    const Opt = { key: 'test1key', value: 'test1val' };
+    const Expected = null;
 
     const Result = component.getOptKey(Opt);
 
@@ -79,6 +93,15 @@ describe('DropdownComponent', () => {
     expect(Result).toEqual(Expected);
   }));
 
+  it('tests the getOptValue function with invalid valueKey', fakeAsync( () => {
+    const Opt = { key: 'test1key', value: 'test1val' };
+    const Expected = null;
+
+    const Result = component.getOptValue(Opt);
+
+    expect(Result).toEqual(Expected);
+  }));
+
   it('tests the isSelected function with valid currentValue', fakeAsync( () => {
     const Opt = { key: 'test1key', value: 'test1val' };
     const Expected = 'selected';
@@ -91,7 +114,7 @@ describe('DropdownComponent', () => {
 
   }));
 
-  it('tests the isSelected function with valid currentValue', fakeAsync( () => {
+  it('tests the isSelected function with invalid currentValue', fakeAsync( () => {
     const Opt = { key: 'test1key', value: 'test1val' };
     const BadOpt = { key: 'badkey', value: 'badval' };
     const Expected = '';
