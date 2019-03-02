f660ced348fe81e8b010c8ff759a2fcda32b0cd3
Cyrielle AM <cyrielle.ameka@laposte.net>
2019-03-03 00:00:45 +0100

    test(loader): Ajout des tests du composant loader

--- src/app/components/loader/loader.component.spec.ts
+++ src/app/components/loader/loader.component.spec.ts
@@ -1,29 +1,30 @@
 import { async, ComponentFixture, TestBed, tick, fakeAsync} from '@angular/core/testing';
 import { LoaderComponent } from './loader.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateService } from '@ngx-translate/core';
 import { MockTranslateService } from '../../models/MockTranslateService';
 import { ThemePreferencesService } from '../../providers/theme-preferences.service';
 import { MockThemePreferencesService } from '../../models/MockThemePreferencesService';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { load } from '@angular/core/src/render3';
+import { MockTranslateLoader } from '../../models/MockTranslateLoader';
 
 describe('LoaderComponent', () => {
+    /* tslint:disable */
     let component: LoaderComponent;
     let fixture: ComponentFixture<LoaderComponent>;
-    let loaderEl: DebugElement;
 
+    /* tslint:enable */
     beforeEach(async(() => {
       TestBed.configureTestingModule({
         declarations: [LoaderComponent],
         imports: [
+          TranslateModule.forRoot({
+            loader: {provide: TranslateLoader, useClass: MockTranslateLoader},
+          })
         ],
         providers: [
-          {
-            provide: TranslateService,
-            useClass: MockTranslateService
-          },
           {
             provide: ThemePreferencesService,
             useClass: MockThemePreferencesService
@@ -36,17 +37,17 @@ describe('LoaderComponent', () => {
     beforeEach(() => {
       fixture = TestBed.createComponent(LoaderComponent);
       component = fixture.componentInstance;
-      loaderEl = fixture.debugElement.query(By.css('h2.gh-loader_content'));
+
     });
 
     it('tests the component creation', () => {
       expect(component).toBeTruthy();
     });
 
-    // it('tests the component loading value', () => {
-    //   component.loading = true;
-    //   fixture.detectChanges();
-    //   expect(component.loading).toBe(true);
-    // });
+    it('tests the component loading value', () => {
+      component.loading = true;
+      fixture.detectChanges();
+      expect(component.loading).toBe(true);
+    });
 
 });
