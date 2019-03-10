1c02f595c565bed1d3bf99c8407122c86e594394
Julien BESNIER <besnierjulien76@gmail.com>
2019-03-10 21:30:38 +0100

    test(sync-2): ajout de l'import

--- src/app/screens/home/home-pull.component.spec.ts
+++ src/app/screens/home/home-pull.component.spec.ts
@@ -14,6 +14,7 @@ import { ButtonComponent } from '../../components/button/button.component';
 import { ModalComponent } from '../../components/modal/modal.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { IconButtonComponent } from '../../components/icon-button/icon-button.component';
+import { CommitTextAreaComponent } from '../../components/commit-text-area/commit-text-area.component';
 import { MatTabsModule, TooltipComponent } from '@angular/material';
 import { ResizableModule, ResizeEvent } from 'angular-resizable-element';
 import { LoaderComponent } from '../../components/loader/loader.component';
@@ -54,6 +55,7 @@ describe('HomeComponent', () => {
         ModalComponent,
         FooterComponent,
         IconButtonComponent,
+        CommitTextAreaComponent,
         LoaderComponent,
         InfoBarComponent,
         AccordionComponent,
